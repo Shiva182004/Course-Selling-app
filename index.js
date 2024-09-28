cee0e83765bbb20ba36b1f1b3a639d87ce32a20f
Shivam Sharma <shivasharmarag@gmail.com>
2024-09-28 16:12:49 +0530

    Step-2 add skeleton routes for user

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.post("/login", function(req, res) {
+
+});
+
+app.signup("/signup", function(req, res) {
+
+});
+
+app.post("/login", function(req, res) {
+
+});
