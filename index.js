9a8902365657debb8211bf3ab7583a9c29694d1a
Shivam Sharma <shivasharmarag@gmail.com>
2024-09-28 17:02:17 +0530

    Added admin route

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
-
+const { adminRouter } = require("./routes/admin");
 const app = express();
 
 app.use("/user", userRouter);
+app.use("/admin", adminRouter);
 app.use("/course", courseRouter);
 
 app.listen(3000);
