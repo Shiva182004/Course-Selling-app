7e2149cce500e7742ef1c3e4b83e0d4dff7348ba
Shivam Sharma <shivasharmarag@gmail.com>
2024-09-29 19:56:41 +0530

    step-10, add auth middleware, implemented course post ep

---
+++ config.js
@@ -0,0 +1,7 @@
+const JWT_USER_PASSWORD = "Shivam@2004";
+const JWT_ADMIN_PASSWORD = "123l123l123l";
+
+module.exports = {
+    JWT_ADMIN_PASSWORD,
+    JWT_USER_PASSWORD
+}
--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
 const adminRouter = Router();
-const { adminModel } = require("../db");
-const JWT_ADMIN_PASSWORD = "123l123l123l";
+const { adminModel, courseModel } = require("../db");
+const { JWT_ADMIN_PASSWORD } = require("../config");
 const jwt = require("jsonwebtoken");
+const { adminMiddleware } = require("../middleware/admin");
 
 adminRouter.post("/signup", async function(req, res) {
     const { email, password, firstName, lastName } = req.body;
@@ -42,9 +43,18 @@ adminRouter.post("/signin", async function(req, res) {
     }
 });
 
-adminRouter.post("/course", function(req, res) {
+adminRouter.post("/course", adminMiddleware, async function(req, res) {
+    const adminId = req.userId;
+
+    const { title, description, imageUrl, price } = req.body;
+
+    //creating a web3 saas in 6 hours
+    const course = await courseModel.create({
+        title, description, imageUrl, price, creatorId: adminId
+    })
     res.json({
-        message:"signin endpoint"
+        message:"Course Created",
+        courseId: course._id
     })
 });
 
